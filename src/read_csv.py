afe7420b27e28192a6855da3edd139d54163b801
benjaminjohansen <benjoh@dtu.dk>
2025-11-16 18:27:27 +0100

    made everything work

--- src/read_csv.py
+++ src/read_csv.py
@@ -7,9 +7,10 @@ participants = pl.read_csv(file_path, encoding="8859", separator=";")
 
 emails = list(participants["Email"])
 
-[print(f"{mail}; ") for mail in emails]
+# [print(f"{mail}; ") for mail in emails]
 
-def find_duplicates(files:list, columns:list=["Email"])->list:
+
+def find_duplicates(files: list, columns: list = ["Email"]) -> list:
     """Find participants who participated in multiple events based on selected column(s). Can be combined multiple times to do lookups across multiple columns.
 
     Args:
@@ -19,18 +20,28 @@ def find_duplicates(files:list, columns:list=["Email"])->list:
         list: Returns a list with the unique duplicates. Can easily be count
     """
 
-    df_all = pl.DataFrame()
-    
+    df_all = []
+
     if len(files) > 1:
         for file in files:
-            df = pl.read_csv(file,encoding="8859", separator=";" )
+            df = (
+                pl.read_csv(file, encoding="8859", separator=";", skip_rows=1)
+                .select(
+                    ["Email", "Antal", "Tilmeldingstidspunkt", "Afmeldingstidspunkt"]
+                )
+                .filter(pl.col("Afmeldingstidspunkt").is_null())
+            )
+            df_all.append(df)
 
-            df_all = pl.union([df_all, df])
     else:
-        df_all =pl.read_csv(files[0],encoding="8859", separator=";" ) 
+        df_all = pl.read_csv(files[0], encoding="8859", separator=";", skip_rows=1)
 
     # find duplicates
-    n_duplicates =df_all.select(pl.col(columns)).unique().height
-    print(n_duplicates)
+    df_final = pl.concat(df_all)
+    n_duplicates = df_final.select(pl.col("Email")).filter(
+        pl.col("Email").is_duplicated()
+    ).unique()
+    print(n_duplicates.height)
 
-find_duplicates([file_path])
+
+find_duplicates(["data/Deltagere - 361185.csv", "data/Deltagere - 361187.csv"])

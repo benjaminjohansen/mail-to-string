84a6ee6478f13a8bb83504d969f13cb5815db9bf
BitsNBen <benjamin.lebiecka@neurospace.io>
2025-12-16 21:10:52 +0100

    added skip row to read_csv

--- src/read_csv.py
+++ src/read_csv.py
@@ -1,9 +1,14 @@
 import polars as pl
 
-file_path = "data/Deltagere - 361222.csv"
+file_path = "data/Deltagere -361222.csv"
 
 # You can use file on mac/linux to find the correct encoding
-participants = pl.read_csv(file_path, encoding="8859", separator=";")
+participants = pl.read_csv(
+    file_path,
+    encoding="8859",
+    separator=";",
+    skip_rows=1,
+)
 
 emails = list(participants["Email"])
 
